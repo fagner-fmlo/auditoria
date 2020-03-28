b8d4b5dcdbef0625475ae8ac8f9fbbcd92b7420a
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-03-28 19:52:20 -0300

    Update brutekiller.sh

--- brutekiller.sh
+++ brutekiller.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-}
-function brutekiller() {
-        brutecheck | awk '{print $11}' | sort | uniq | while read ip;do csf -d $ip;done
-}
+awk '{print $11}' | sort | uniq | while read ip;do csf -d $ip;done
 
