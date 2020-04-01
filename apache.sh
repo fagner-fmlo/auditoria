c3950aae575841394b26c7329fc36b20fa599735
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-01 16:22:30 -0300

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -4,9 +4,10 @@
 # Copyright 2020							                                                
 # Author: Fagner Mendes							                                          
 # License: GNU Public License						                                      
-# Version: 1.1								                                                  
+# Version: 1.2							                                                  
 # Email: fagner.mendes22@gmail.com					                                  
 ###############################################################################
 
 apachectl fullstatus | grep "http/1.1" | awk {'print $14'} | sort | uniq -c | sort -n | tail -5
 cat /usr/local/apache/logs/access_log |  grep "http/1.1" | awk {'print $14'} | sort | uniq -c | sort -n | tail -5 | netstat -tunelap|egrep -v 'TIME_W|LIST'|awk '/:80|:443/{print $5}'|cut -d: -f1 | sort | uniq -c | sort -nk 1 | tail -20
+sed -i '$d' /root/.bash_history
