1db28e4787550e58e81af29cef0f34c1e3399bdf
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-21 12:29:51 -0300

    Create analisysprocess.sh

---
+++ analisysprocess.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###############################################################################
+# Copyright 2020							                                                
+# Author: Fagner Mendes							                                          
+# License: GNU Public License						                                      
+# Version: 1.0								                                                  
+# Email: fagner.mendes22@gmail.com					                                  
+###############################################################################
+
+#This is system monitor process and serves to monitor the load of each process for a given service in the system
+
+
+echo "Inform the number PID that you want analisys"
+read pid
+
+echo "Inform the number in seconds to this analisy"
+read seconds
+
+pidstat -p $pid $seconds
