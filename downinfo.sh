5c9d3f0e8d895cf0e24649db915d3ab36d704ba2
Fagner Mendes <60005494+fagner-fmlo@users.noreply.github.com>
2020-04-30 14:24:40 -0300

    Update downinfo.sh

--- downinfo.sh
+++ downinfo.sh
@@ -16,7 +16,7 @@ cd /home/$cpuser/public_html/
 wget https://raw.githubusercontent.com/fagner-fmlo/arquivos/master/info.php
 user=$(pwd | cut -d/ -f3)
 find $user -type f -exec chown $user.$user {} +
-echo "Press <Enter> To continue and delete file info.php"
+echo "Press <Enter> To continue and delete the file info.php"
 read
-rm info.php
+rm -f info.php
 echo -e "\033[01;35mSay Hello to Brazil"
